45aa0cab28e43241a16dc1bc46467e082888ac32
fdel15 <frankdelpidio@gmail.com>
2023-05-19 15:15:41 -0400

    Creates embeddings_generator model

--- app/models/embeddings_generator.rb
+++ app/models/embeddings_generator.rb
@@ -3,9 +3,10 @@
 class EmbeddingsGenerator
   include CsvHelpers
   EMBEDDINGS_DIRECTORY = "#{Rails.root}/embeddings"
+  DEFAULT_EMBEDDER = OpenAi::Embeddings.new
 
   attr_reader :embedder
-  def initialize(embedder: OpenAI::Embedder.new)
+  def initialize(embedder: DEFAULT_EMBEDDER)
     @embedder = embedder
   end
 
@@ -22,13 +23,18 @@ class EmbeddingsGenerator
   end
 
   def create_embeddings_csv(
-    data_csv_file_path:,
+    pages_csv_file_path:,
     output_file_name:,
     output_directory: EMBEDDINGS_DIRECTORY
   )
-    # headers = title + 0 --> max vector columns
-    # data = get embedding
-    # file_path = "#{output_directory}/#{title}.csv"
+    data = get_embeddings_from_pages(pages_csv_file_path)
+    file_path = "#{output_directory}/#{output_file_name}"
+
+    write_to_csv(
+      data: data,
+      file_path: file_path,
+      headers: embeddings_csv_headers
+    )
   end
 
   private
@@ -58,9 +64,36 @@ class EmbeddingsGenerator
     [page_number, text, calculate_tokens(text)]
   end
 
-  # We should make the encoding an ENV to easily switch
   def calculate_tokens(text)
-    encoder = Tiktoken.get_encoding("cl100k_base")
-    encoder.encode(text).length
+    embedder.calculate_tokens(text)
+  end
+
+  def embeddings_csv_headers
+    num_of_vectors = embedder.output_dimensions
+    vector_columns = (0..num_of_vectors - 1).to_a
+    ["Page #"] + vector_columns
+  end
+
+  # Assumes pages to have the following headers:
+  # ["Page #", "Content", "# of Tokens"]
+  def get_embeddings_from_pages(file_path)
+    embeddings = []
+    CSV.foreach(file_path, headers: true) do |row|
+      page_number = row["Page #"]
+      content = row["Content"]
+
+      embedding = get_embedding(content)
+      embeddings << [page_number] + embedding
+
+    rescue OpenAi::MaxTokenError
+      # TODO: It would be better to do more than rescue this error by logging
+      puts "Skipping #{page_number} because its token count is greater than the max token count"
+    end
+
+    embeddings
+  end
+
+  def get_embedding(text)
+    embedder.create(text)
   end
 end
--- test/models/embeddings_generator_test.rb
+++ test/models/embeddings_generator_test.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require_relative "../helpers/open_ai_helpers"
 
 class TestEmbeddingsGenerator < ActiveSupport::TestCase
   def setup
     @embeddings_generator = EmbeddingsGenerator.new
     @sample_pdf = "#{Rails.root}/test/fixtures/files/sample.pdf"
     @sample_pages_csv = "#{Rails.root}/test/fixtures/files/sample.csv"
-    @sample_embeddings_csv = "#{Rails.root}/test/fixtures/files/embeddings.csv"
     @output_directory = "#{Rails.root}/test/models/test_data"
   end
 
@@ -41,26 +41,30 @@ class TestEmbeddingsGenerator < ActiveSupport::TestCase
   end
 
   def test_create_embeddings_csv
-    data_csv_file_path = @sample_pages_csv
-    output_file_name = Faker::Lorem.word
+    pages_csv_file_path = @sample_pages_csv
+    output_file_name = "#{Faker::Lorem.word}.csv"
     output_directory = @output_directory
+    embedder = @embeddings_generator.embedder
+
+    OpenAiHelpers::Embeddings.mock_embeddings_request(embedder.client)
 
     @embeddings_generator.create_embeddings_csv(
-      data_csv_file_path: data_csv_file_path,
+      pages_csv_file_path: pages_csv_file_path,
       output_file_name: output_file_name,
       output_directory: output_directory
     )
 
-    output_file = "#{@output_directory}/#{title}.csv"
+    output_file = "#{@output_directory}/#{output_file_name}"
 
     assert File.exist?(output_file), "Should generate CSV output file"
 
+    lines = CSV.readlines(output_file)
+    output_dimensions = embedder.output_dimensions
 
-    output_file_hash = Digest::SHA256.file(output_file).hexdigest
-
-    expected_file_hash = Digest::SHA256.file(@sample_embeddings_csv).hexdigest
-
-    assert_equal(output_file_hash, expected_file_hash)
+    assert_equal(
+      lines.first.length, output_dimensions + 1, 
+      "Output file should contain column for each output dimension plus a title column"
+    )
   end
 end
 
