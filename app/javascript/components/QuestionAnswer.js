62d3ba88062809d1adcb20df077b0edaec16992a
fdel15 <frankdelpidio@gmail.com>
2023-05-20 10:37:38 -0400

    Use arrow functions to bind this in QuestionForm component

--- app/javascript/components/QuestionAnswer.js
+++ app/javascript/components/QuestionAnswer.js
@@ -1,12 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 class QuestionAnswer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickUrl: this.props.clickUrl || "/", // default to root route
+    };
+  }
+
+  handleClick = () => {
+    // console.log("Handle Click rootUrl: ", this.rootUrl);
+    window.location.href = this.state.clickUrl;
+  };
+
   render() {
     return (
       <React.Fragment>
         <p id="answer-container">
           <strong>Answer:</strong> <span id="answer">{this.props.answer}</span>
-          <button id="ask-another-button" style={{ display: "block" }}>
+          <button
+            id="ask-another-button"
+            style={{ display: "block" }}
+            onClick={this.handleClick}
+          >
             Ask another question
           </button>
         </p>
