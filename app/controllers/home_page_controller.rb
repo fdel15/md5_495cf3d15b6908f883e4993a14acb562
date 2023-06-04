be9bfa7feb90e5700c505c65a7a678b4d9c7b46d
fdel15 <frankdelpidio@gmail.com>
2023-06-04 09:52:57 -0400

    Makes book-image configurable with an ENV

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -2,11 +2,16 @@ class HomePageController < ApplicationController
   def index
     @data_file = embeddings_file_path
     @faqs = Question.faqs
+    @book_image = book_image
   end
 
   private
 
   def embeddings_file_path
     ENV["EMBEDDINGS_FILE_PATH"] || "#{Rails.root}/embeddings/embeddings.csv"
+  end
+
+  def book_image
+    ENV.fetch("BOOK_IMAGE") { 'book-covers.jpg' }
   end
 end
